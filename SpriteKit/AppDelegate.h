6d6ac8123ed77f98abfb062823f516baaadecb3f
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-06-13 18:56:21 -0700

    Initial Commit

---
+++ SpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SpriteKit
+//
+//  Created by Zakk Hoyt on 6/13/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SpriteKitTests/SpriteKitTests.m
@@ -0,0 +1,39 @@
+//
+//  SpriteKitTests.m
+//  SpriteKitTests
+//
+//  Created by Zakk Hoyt on 6/13/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SpriteKitTests : XCTestCase
+
+@end
+
+@implementation SpriteKitTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
